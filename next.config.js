d878c0a5ab33e050a3ed53ca2abefc9cbdf20f69
Winston Hoy <winston@commonenergy.us>
2021-11-30 15:18:03 -0500

    add next.config.js for splitting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-const withSplit = require('next-with-split')({});
+const withSplit = require('next-with-split')({
+  splits: {
+   canary: {
+     hosts: {
+       original: 'https://next-with-split-demo.vercel.app/',
+       canary: 'https://next-with-split-demo-git-canary.vercel.app/',
+     },
+   },
+ },
+});
 
 module.exports = withSplit({
   reactStrictMode: true,
