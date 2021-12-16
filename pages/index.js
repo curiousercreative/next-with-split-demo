2a0fe0cfdd48f7681027151ad2f01ef8159fe82b
Winston Hoy <winston@commonenergy.us>
2021-12-16 13:57:11 -0500

    vary

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        This is the original
+        This is the canary!
       </main>
     </div>
   )
